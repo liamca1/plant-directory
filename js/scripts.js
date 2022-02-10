b1ee197a4199823cb0853a0b541137439049c654
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-02-10 14:31:31 +0100

    First Creation

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert('Hello world');
+
+// To practice working with variables, create a variable named favoriteFood and assign it a value of your choice.
+// Use document.write() to display what’s stored in the favoriteFood variable on the index page.
+
+let favoriteFood = 'potatoes'
+document.write(favoriteFood)
