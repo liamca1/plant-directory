bf91df26a3286e0628286a56752677f2f495f71c
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-02-14 15:38:19 +0100

    3 Objects Created
    Three objects (New Zealand Mushrooms) have been created with data obtained from New Zealand Landcare Research Fungi database.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// list of New Zealand Mushrooms //
+let mushroomList = {
+    phlegmacium : {taxonName : "Phlegmacium" , collectionYear : 1968 , types : ['Soil' , 'Otago Lakes' , 'Forest']} ,
+    agaricalesClem : {taxonName : "Agaricales Clem" , collectionYear : 1983 , types : ['Litter' , 'Auckland' , 'Forest']} ,
+    boletaceae : {taxonName : "Boletaceae" , collectionYear : 1970 , types : ['soil' , 'Nelson' , 'Forest']} ,
+}
+
+
+
+
+//  mushroomName : {taxonName : "mushroom name" , collectionYear : 0000 , types : ['substrate' , 'location' , 'habitat']} ,
