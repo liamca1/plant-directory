8484b6aee608b85be8b145dde7105175c0ac9152
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-02-16 11:36:41 +0100

    Loop iterating through mushRoom list update
    Also added a conditional (if mushroom was identified after 1980, then a message is displayed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 // list of New Zealand Mushrooms //
 let mushroomList = [
-    { taxonName: "Phlegmacium", collectionYear: 1968, types: ['Soil', 'Otago Lakes', 'Forest'] },
-    { taxonName: "Agaricales Clem", collectionYear: 1983, types: ['Litter', 'Auckland', 'Forest'] },
-    { taxonName: "Boletaceae", collectionYear: 1970, types: ['soil', 'Nelson', 'Forest'] }
+    { taxonName: "Phlegmacium", collectionYear: 1968, habitat: ['Soil', 'Otago Lakes', 'Forest'] },
+    { taxonName: "Agaricales Clem", collectionYear: 1983, habitat: ['Litter', 'Auckland', 'Forest'] },
+    { taxonName: "Boletaceae", collectionYear: 1970, habitat: ['soil', 'Nelson', 'Forest'] }
 ];
 
+// iterates through mushroomList //
+for (let i=0; i < mushroomList.length; i++) {
+    if(mushroomList[i].collectionYear > 1980){
+        document.write(mushroomList[i].taxonName + "( year identified : " + mushroomList[i].collectionYear + " )" + "This mushroom was only identified recently!")
+    }else {
+    document.write(mushroomList[i].taxonName + " (year identified : " + mushroomList[i].collectionYear + ") " + " (habitat : " + mushroomList[i].habitat + ")")  
+    }
+}
 
 
 
-//  mushroomName : {taxonName : "mushroom name" , collectionYear : 0000 , types : ['substrate' , 'location' , 'habitat']} ,
+//  mushroomName : {taxonName : "mushroom name" , collectionYear : 0000 , habitat : ['substrate' , 'location' , 'ecosystem']} ,
+
+// if(mushroomList[i].collectionYear > 1){
+
