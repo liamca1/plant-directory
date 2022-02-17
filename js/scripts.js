a27798d8486f98bc1d168236c582338b34f24746
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-02-17 13:27:21 +0100

    Line breaks added

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,9 @@ let mushroomList = [
 // iterates through mushroomList //
 for (let i=0; i < mushroomList.length; i++) {
     if(mushroomList[i].collectionYear > 1980){
-        document.write(mushroomList[i].taxonName + "( year identified : " + mushroomList[i].collectionYear + " )" + "This mushroom was only identified recently!")
+        document.write(mushroomList[i].taxonName + "( year identified : " + mushroomList[i].collectionYear + " )" + "This mushroom was only identified recently! <br>")
     }else {
-    document.write(mushroomList[i].taxonName + " (year identified : " + mushroomList[i].collectionYear + ") " + " (habitat : " + mushroomList[i].habitat + ")")  
+    document.write(mushroomList[i].taxonName + " (year identified : " + mushroomList[i].collectionYear + ") " + " (habitat : " + mushroomList[i].habitat + ")<br>")  
     }
 }
 
@@ -18,5 +18,26 @@ for (let i=0; i < mushroomList.length; i++) {
 
 //  mushroomName : {taxonName : "mushroom name" , collectionYear : 0000 , habitat : ['substrate' , 'location' , 'ecosystem']} ,
 
-// if(mushroomList[i].collectionYear > 1){
+/* functions to solve multiple lists problem.
 
+let pokemonList = [
+  // Pokémon objects
+];
+
+let pokemonList2 = [
+  // different set of Pokémon objects
+];
+
+function printArrayDetails(list){
+  for (let i = 0; i < list.length; i++){
+    // document.write("<p>" + list[i].name + "</p>");
+    // printing list[i]’s other details
+    // ...
+  }
+}
+
+printArrayDetails(pokemonList); // executes the function using ‘pokemonList‘ as its input
+
+printArrayDetails(pokemonList2); // executes the function using ‘pokemonList2‘ as its input
+
+*/
